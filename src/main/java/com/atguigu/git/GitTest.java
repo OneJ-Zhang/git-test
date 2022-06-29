8c7a62f2c80c4cf1f9120f982abbe0db0803bbae
Tom <YJtom0813@outlook.com>
2022-06-29 19:27:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 张一杰
+ * @version 1.0
+ * @time 2022/6/29 19:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
